7a348a8ae84744f577c6f8cddcefae0df882e206
Adam Marley <141704661+marleyad@users.noreply.github.com>
2024-10-17 22:20:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
-
 
 @app.route("/")
 def ask_questions():
